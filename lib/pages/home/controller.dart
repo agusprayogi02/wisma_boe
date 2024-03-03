413df4233fb5e27c9038c87b6dbed4bf316c27c0
Agus Prayogi <agus21apy@gmail.com>
2024-03-03 21:21:41 +0700

    [ADD] nfc detector

---
+++ lib/pages/home/controller.dart
@@ -0,0 +1,67 @@
+part of 'page.dart';
+
+class HomeController {
+  ValueNotifier<dynamic> result = ValueNotifier(null);
+
+  void tagRead() {
+    print("Halo semua");
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      if (tag.data['isodep']['identifier'] != null) {
+        result.value = HEX.encode(tag.data['isodep']['identifier']);
+        NfcManager.instance.stopSession();
+        return;
+      }
+      result.value = tag.data;
+      NfcManager.instance.stopSession();
+    });
+  }
+
+  void ndefWrite() {
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = 'Tag is not ndef writable';
+        NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+
+      NdefMessage message = NdefMessage([
+        NdefRecord.createText('Hello World!'),
+        NdefRecord.createUri(Uri.parse('https://flutter.dev')),
+        NdefRecord.createMime('text/plain', Uint8List.fromList('Hello'.codeUnits)),
+        NdefRecord.createExternal('com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
+      ]);
+
+      try {
+        await ndef.write(message);
+        result.value = 'Success to "Ndef Write"';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        result.value = e;
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    });
+  }
+
+  void ndefWriteLock() {
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+      var ndef = Ndef.from(tag);
+      if (ndef == null) {
+        result.value = 'Tag is not ndef';
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+
+      try {
+        await ndef.writeLock();
+        result.value = 'Success to "Ndef Write Lock"';
+        NfcManager.instance.stopSession();
+      } catch (e) {
+        result.value = e;
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        return;
+      }
+    });
+  }
+}
